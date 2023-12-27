59e7a0a71160bae114fe13328297d65c9c3d6fde
OliaMedinska <medinska2@gmail.com>
2023-12-27 11:33:47 +0100

    contacts fixed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,24 +15,30 @@ export const Contact = ({ contacts, filter, onDeleteContact }) => {
   const filtredContacts = getVisibleContacts();
 
   return (
-    <ul>
-      {filtredContacts.map(contact => {
-        const { id, name, number } = contact;
-        return (
-          <li key={id} className="contact-item">
-            <p className="contact-text">
-              {name}: {number}
-            </p>
-            <button
-              className="contact-button"
-              type="button"
-              onClick={() => deleteContactHandler(id)}
-            >
-              Delete
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {filtredContacts.length !== 0 ? (
+        <ul>
+          {filtredContacts.map(contact => {
+            const { id, name, number } = contact;
+            return (
+              <li key={id} className="contact-item">
+                <p className="contact-text">
+                  {name}: {number}
+                </p>
+                <button
+                  className="contact-button"
+                  type="button"
+                  onClick={() => deleteContactHandler(id)}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p className="heading">No contacts</p>
+      )}
+    </>
   );
 };
